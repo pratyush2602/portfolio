3aa1c3eb7a8e1a8491e494d49498cb8ad1ac0a63
pratyush2602 <pratyush2602@gmail.com>
2024-12-31 16:43:45 +0530

    server

--- backend/app.py
+++ backend/app.py
@@ -22,8 +22,3 @@ def read_root():
     with open("frontend\index.html") as f:
         return HTMLResponse(f.read())
 
-
-import uvicorn
-
-if __name__ == "__main__":
-    uvicorn.run(app,host="0.0.0.0",port=8000)
